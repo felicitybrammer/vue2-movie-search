04ab54a61d0861fd08e60f81952c6e37081a9f79
Felicity Brammer <felicitybrammer@gmail.com>
2023-05-29 13:26:19 -0700

    updates to api call

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
-    <form class="search-form" @submit.prevent="search">
-      <label for="movie-title">Search for a movie:</label>
+    <form class="search-form" id="movie-title" @submit.prevent="search">
+      <label for="movie-title" >Search for a movie:</label>
+    
       <input type="text" v-model="movieTitle">
-      <router-link :to="{ name: 'results' }"><button>Search</button></router-link>
+      <router-link :to="{ name: 'results' }">
+        <button>Search</button>
+      </router-link>
+
     </form>
   </div>
 </template>
@@ -17,6 +21,12 @@
     }
   }
 </script>
+
+<style>
+  #movie-title {
+    padding: 10px;
+  }
+</style>
 
 
 
